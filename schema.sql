1b22a112cb583aecabde7a97589cdff60b633323
Anawin Pongsaboripat <anawin.mylon@gmail.com>
2016-06-23 10:30:31 +0700

    Add create table script in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS hospitals;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE hospitals
 (
@@ -12,3 +13,22 @@ INSERT INTO hospitals(id
     , name)
 VALUES (0
     , 'test hospital name');
+
+CREATE TABLE users
+(
+    id              BIGINT NOT NULL UNIQUE,
+    name            VARCHAR(256) NOT NULL,
+    surname         VARCHAR(256) NOT NULL,
+    email           VARCHAR(256) NOT NULL UNIQUE,
+    CONSTRAINT      users_pk PRIMARY KEY (id)
+);
+ALTER TABLE users OWNER TO begdoctor;
+
+INSERT INTO users(id
+    , name
+    , surname
+    , email)
+VALUES (0
+    , 'patientName'
+    , 'patientSurname'
+    , 'patient@mail.com');
