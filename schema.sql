a8f606fb4fe34f235bc7ed2dc57ce7b86168bfa0
champillon <passapong.t@gmail.com>
2016-07-05 19:42:03 +0700

    เพิ่ม url , location , doctorName ใน Database script พร้อม Data ตัวอย่าง

--- schema.sql
+++ schema.sql
@@ -5,14 +5,23 @@ CREATE TABLE hospitals
 (
     id              BIGINT NOT NULL UNIQUE,
     name            VARCHAR(256) NOT NULL UNIQUE,
+    url             VARCHAR(256) NOT NULL UNIQUE,
+    location        VARCHAR(256) NOT NULL UNIQUE,
+    doctorName      VARCHAR(256) NOT NULL UNIQUE,
     CONSTRAINT      accounts_pk PRIMARY KEY (id)
 );
 ALTER TABLE hospitals OWNER TO begdoctor;
 
 INSERT INTO hospitals(id
-    , name)
+    , name
+    , url
+    , location
+    , doctorName )
 VALUES (0
-    , 'test hospital name');
+    , 'test hospital name'
+    , 'https://www.benin2009.com/wp-content/uploads/2015/11/hospital.png'
+    , 'Seoul Korea'
+    , 'Kang Moyeon' );
 
 CREATE TABLE users
 (
