62e628d556d2861f615afd653f489f6dbfba1dff
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-08 15:14:05 +0700

    สร้าง Controller ของ Symptom

---
+++ app/controllers/api/SymptomController.scala
@@ -0,0 +1,18 @@
+package controllers.api
+
+import com.google.inject.{Inject, Singleton}
+import facades.SymptomFacade
+import play.api.mvc._
+
+/**
+  * Created by Siam yimyam on 8/7/2559.
+  */
+@Singleton
+class SymptomController @Inject() (symptomFacade: SymptomFacade)
+  extends Controller {
+
+  def getSymptom = Action {
+    Ok(symptomFacade.listAllSymptom.toText)
+  }
+
+}
