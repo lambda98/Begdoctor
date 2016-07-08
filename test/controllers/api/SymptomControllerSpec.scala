54362ea32176deda109826311c8411ed1398b91d
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-08 15:16:24 +0700

    สร้าง ControllerSpec ของ Symptom

---
+++ test/controllers/api/SymptomControllerSpec.scala
@@ -0,0 +1,23 @@
+package controllers.api
+
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+/**
+  * Created by Siam yimyam on 8/7/2559.
+  */
+class SymptomControllerSpec
+  extends PlaySpec
+    with OneAppPerTest {
+
+   "GET /api/v1/symptoms/1" should {
+     "return Hospital JSON" in {
+       contentAsString(route(app, FakeRequest(GET, "/api/v1/symptom")).get) must include
+       """
+         |"name":"test symptom name"
+       """.stripMargin
+     }
+   }
+}
+
