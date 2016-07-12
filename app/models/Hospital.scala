124c0bb37d8568d1a70b9d5280e781ddf4f77b3b
Pumpuy Supaporn <p.pumpuy94@gmail.com>
2016-07-12 13:42:16 +0700

    เพิ่มข้อมูล types,latitude,longitude,available_time ใน Hospital

--- app/models/Hospital.scala
+++ app/models/Hospital.scala
@@ -9,5 +9,9 @@ case class Hospital(id: Long
                     , name: String
                     , url: String
                     , location: String
-                    , doctorName: String)
+                    , doctorName: String
+                    , types: String
+                    , latitude: String
+                    , longitude: String
+                    , available_time: String)
   extends Json
