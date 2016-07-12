435f57f3721e3fad738b313c6e9275b3e3acf193
Pumpuy Supaporn <p.pumpuy94@gmail.com>
2016-07-12 13:48:32 +0700

    เพิ่มข้อมูล types,latitude,longitude,available_time ใน HospitalPersistSpec

--- test/persists/HospitalPersistSpec.scala
+++ test/persists/HospitalPersistSpec.scala
@@ -12,7 +12,15 @@ class HospitalPersistSpec
   "Call findById" should {
     "return Hospital of that id" in {
       val correct_id = 0L
-      val correct_name = "test hospital name"
+      val correct_name = "Hae Song"
+      val coreect_url = "https://www.benin2009.com/wp-content/uploads/2015/11/hospital.png"
+      val correct_location = "Seoul Korea"
+      val correct_doctorName = "Kang Moyeon"
+      val correct_types = "Hospital"
+      val correct_latitude = "13.7854529"
+      val correct_longitude = "100.5736408"
+      val correct_available_time = "10:00 - 17:00"
+
       val persist = app.injector.instanceOf[HospitalPersist]
 
       val testObject = persist.findById(correct_id)
