98de767cd773395b24443eaef5cdcc0c2b468f4f
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-13 14:48:10 +0700

    แก้ไข String เป็น boolean

--- app/models/Hospital_Time.scala
+++ app/models/Hospital_Time.scala
@@ -6,9 +6,9 @@ import utilities.Json
 /**
   * Created by Siam yimyam on 13/7/2559.
   */
-class Hospital_Time (id: Long
+case class Hospital_Time (id: Long
                      , hospital_id: Long
                      , start_datetime: DateTime
                      , finish_datetime: DateTime
-                     , available: String)
+                     , available: Boolean)
   extends Json
