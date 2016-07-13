17c1ddc76646746ae1039016aca8d734dcf01e37
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-13 14:48:33 +0700

    แก้ไข String เป็น boolean

--- app/persists/postgres/Hospital_TimePostgres.scala
+++ app/persists/postgres/Hospital_TimePostgres.scala
@@ -1,6 +1,5 @@
 package persists.postgres
 
-import java.lang.annotation.Annotation
 import java.sql.ResultSet
 import javax.inject.{Inject, Singleton}
 
@@ -29,9 +28,9 @@ class Hospital_TimePostgres @Inject() (db: Database)
   private[postgres] def parse(resultSet: ResultSet): Hospital_Time = Hospital_Time(
     id = resultSet.getLong("id")
     , hospital_id = resultSet.getLong("hospital_id")
-    , start_datetime = new DateTime(resultSet.getTimestamp("2016-07-01 10:30").getTime)
-    , finish_datetime = new DateTime(resultSet.getTimestamp("2016-07-02 11:30").getTime)
-    , vailable = resultSet.getString("true")
+    , start_datetime = new DateTime(resultSet.getTimestamp("start_datetime").getTime)
+    , finish_datetime = new DateTime(resultSet.getTimestamp("finish_datetime").getTime)
+    , available = resultSet.getBoolean("available")
   )
 
   private val FIND_BY_ID = "SELECT * FROM hospitals_time where id = ?"
