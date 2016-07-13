f9447a6261673b9a64ce9da06503a1e09af7eef4
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-13 12:14:17 +0700

    สร้าง ModelSpec ของ Hospital_Time

---
+++ test/models/Hospital_TimeSpec.scala
@@ -0,0 +1,38 @@
+package models
+
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+
+/**
+  * Created by Siam yimyam on 13/7/2559.
+  */
+class Hospital_TimeSpec
+  extends PlaySpec
+    with OneAppPerTest {
+
+  "Create new Hospital_Time" should {
+    "return Hospital_Time" in {
+      val correct_id = 1L
+      val correct_hospital_id = 1L
+      val correct_start_datetime = "2016-07-01 10:30"
+      val correct_finish_datetime = "2016-07-01 11:30"
+      val correct_available = "true"
+
+
+
+      val testObject = new Hospital_Time(
+        id = correct_id
+        , hospital_id = correct_hospital_id
+        , start_datetime = correct_start_datetime
+        , finish_datetime = correct_finish_datetime
+        , available = correct_available
+      )
+
+      assert(correct_id == testObject.id)
+      assert(correct_hospital_id == testObject.hospital_id)
+      assert(correct_start_datetime == testObject.start_datetime)
+      assert(correct_finish_datetime == testObject.finish_datetime)
+      assert(correct_available == testObject.available)
+    }
+  }
+
+}
