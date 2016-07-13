859b45fcc41206176a1fbefe4a5eb8c0cc40e80a
champillon <passapong.t@gmail.com>
2016-07-13 21:18:26 +0700

    แก้ test case ที่ fail เพราะดึง id เป็น 0 มา แต่ id เป็น 0 ไม่มีใน db ต้องเปลี่ยน id เป็น 1 แทน

--- test/persists/Hospital_TimePersistSpec.scala
+++ test/persists/Hospital_TimePersistSpec.scala
@@ -1,5 +1,6 @@
 package persists
 
+import org.joda.time.DateTime
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 
 /**
@@ -11,11 +12,7 @@ class Hospital_TimePersistSpec
 
   "Call findByHospital_Id" should {
     "return Hospital of that id" in {
-      val correct_id = 0L
-      val correct_hospital_id = 0L
-      val correct_start_datetime = "2016-07-01 10:30"
-      val correct_finish_datetime = "2016-07-01 11:30"
-      val correct_available = "true"
+      val correct_id = 1L
 
       val persist = app.injector.instanceOf[Hospital_TimePersist]
 
