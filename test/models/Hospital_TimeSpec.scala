2ccb39da9c86ea01f179bfef2e9f8afcce925b4c
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-21 18:07:48 +0700

    เปลี่ยน snake case เป็น camel case

--- test/models/Hospital_TimeSpec.scala
+++ test/models/Hospital_TimeSpec.scala
@@ -10,28 +10,27 @@ class Hospital_TimeSpec
   extends PlaySpec
     with OneAppPerTest {
 
-  "Create new Hospital_Time" should {
-    "return Hospital_Time" in {
+  "Create new HospitalTime" should {
+    "return HospitalTime" in {
       val correct_id = 1L
       val correct_hospital_id = 1L
       val correct_start_datetime = new DateTime(2016,7,1,10,30,0,0)
       val correct_finish_datetime = new DateTime(2016,7,1,11,30,0,0)
       val correct_available = true
 
-      val testObject = new Hospital_Time(
+      val testObject = new HospitalTime(
         id = correct_id
-        , hospital_id = correct_hospital_id
-        , start_datetime = correct_start_datetime
-        , finish_datetime = correct_finish_datetime
+        , hospitalId = correct_hospital_id
+        , startDateTime = correct_start_datetime
+        , finishDateTime = correct_finish_datetime
         , available = correct_available
       )
 
       assert(correct_id == testObject.id)
-      assert(correct_hospital_id == testObject.hospital_id)
-      assert(correct_start_datetime == testObject.start_datetime)
-      assert(correct_finish_datetime == testObject.finish_datetime)
+      assert(correct_hospital_id == testObject.hospitalId)
+      assert(correct_start_datetime == testObject.startDateTime)
+      assert(correct_finish_datetime == testObject.finishDateTime)
       assert(correct_available == testObject.available)
     }
   }
-
 }
