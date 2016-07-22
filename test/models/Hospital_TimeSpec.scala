43f8c476bca3b28bea7dafc6c09e6d6b1bb95a16
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-07-22 22:05:07 +0700

    เปลี่ยนเป็น camel case

--- test/models/Hospital_TimeSpec.scala
+++ test/models/Hospital_TimeSpec.scala
@@ -13,23 +13,23 @@ class Hospital_TimeSpec
   "Create new HospitalTime" should {
     "return HospitalTime" in {
       val correct_id = 1L
-      val correct_hospital_id = 1L
-      val correct_start_datetime = new DateTime(2016,7,1,10,30,0,0)
-      val correct_finish_datetime = new DateTime(2016,7,1,11,30,0,0)
+      val correct_hospitalId = 1L
+      val correct_startDatetime = new DateTime(2016,7,1,10,30,0,0)
+      val correct_finishDatetime = new DateTime(2016,7,1,11,30,0,0)
       val correct_available = true
 
       val testObject = new HospitalTime(
         id = correct_id
-        , hospitalId = correct_hospital_id
-        , startDateTime = correct_start_datetime
-        , finishDateTime = correct_finish_datetime
+        , hospitalId = correct_hospitalId
+        , startDateTime = correct_startDatetime
+        , finishDateTime = correct_finishDatetime
         , available = correct_available
       )
 
       assert(correct_id == testObject.id)
-      assert(correct_hospital_id == testObject.hospitalId)
-      assert(correct_start_datetime == testObject.startDateTime)
-      assert(correct_finish_datetime == testObject.finishDateTime)
+      assert(correct_hospitalId == testObject.hospitalId)
+      assert(correct_startDatetime == testObject.startDateTime)
+      assert(correct_finishDatetime == testObject.finishDateTime)
       assert(correct_available == testObject.available)
     }
   }
