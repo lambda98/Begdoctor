ea9a8bc0eec1cfbc5f633f4ba1b29bd64cf3858e
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-10 12:24:04 +0700

    สร้าง HospitalRetrievalPostgres

---
+++ app/persists/postgres/HospitalRetrievalPostgres.scala
@@ -0,0 +1,36 @@
+package persists.postgres
+
+import java.sql.ResultSet
+import javax.inject.{Inject, Singleton}
+
+import models.HospitalRetrieval
+import persists.HospitalRetrievalPersist
+import play.api.db.Database
+
+/**
+  * Created by Siam yimyam on 9/8/2559.
+  */
+class HospitalRetrievalPostgres @Inject() (db: Database)
+  extends HospitalRetrievalPersist{
+
+  override def insert(lat: String
+                     , lng: String
+                     , name: String): Boolean = db.withConnection { implicit conn =>
+    val preparedStatement = conn.prepareStatement(INSERT)
+    preparedStatement.setString(1,lat)
+    preparedStatement.setString(2,lng)
+    preparedStatement.setString(3,name)
+    preparedStatement.executeUpdate() match {
+      case 1 => true
+      case _ => false
+    }
+  }
+
+  private [postgres] def parse(resultSet: ResultSet): HospitalRetrieval = HospitalRetrieval(
+    lat = resultSet.getString("lat")
+    , lng = resultSet.getString("lng")
+    , name = resultSet.getString("name")
+  )
+
+  private val INSERT = "INSERT INTO hospitals (lat, lng, name) VALUE (?, ?, ?)"
+}
