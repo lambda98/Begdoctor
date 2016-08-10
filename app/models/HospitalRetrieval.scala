103d40badee2be480381883df08c7fb0918725cf
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-10 12:28:59 +0700

    สร้าง HospitalRetrieval

---
+++ app/models/HospitalRetrieval.scala
@@ -0,0 +1,12 @@
+package models
+
+import utilities.Json
+
+
+/**
+  * Created by Siam yimyam on 10/8/2559.
+  */
+case class HospitalRetrieval(lat: String
+                             , lng: String
+                             , name: String)
+  extends Json
