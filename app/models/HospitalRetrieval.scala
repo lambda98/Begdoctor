dfe3a62a8c7929f19f2653618a4a3e7b7cf7946d
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-11 17:11:06 +0700

    เพิ่ม id และ เปลี่ยน String เป็น Float

--- app/models/HospitalRetrieval.scala
+++ app/models/HospitalRetrieval.scala
@@ -6,7 +6,8 @@ import utilities.Json
 /**
   * Created by Siam yimyam on 10/8/2559.
   */
-case class HospitalRetrieval(lat: String
-                             , lng: String
+case class HospitalRetrieval(id: Long
+                             ,lat: Float
+                             , lng: Float
                              , name: String)
   extends Json
