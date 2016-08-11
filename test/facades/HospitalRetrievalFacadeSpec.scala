3554134fa7a966d42af7c9e44f13341fa5ce0eb4
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-11 17:12:32 +0700

    เพิ่ม id และ เปลี่ยน String เป็น Float

--- test/facades/HospitalRetrievalFacadeSpec.scala
+++ test/facades/HospitalRetrievalFacadeSpec.scala
@@ -4,20 +4,33 @@ import javax.inject.Inject
 
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import play.api.inject.guice.GuiceApplicationBuilder
+import services.UuidService
 
 /**
   * Created by Siam yimyam on 11/8/2559.
   */
-class HospitalRetrievalFacadeSpec @Inject()
+class HospitalRetrievalFacadeSpec @Inject()(uuidService: UuidService)
   extends PlaySpec
     with OneAppPerSuite {
 
   implicit override lazy val app = new GuiceApplicationBuilder().build
 
+  "Call findById" should {
+    "return HospitalRetrieval of that id" in {
+      val correct_id = 0L
+      val facade = app.injector.instanceOf[HospitalRetrievalFacade]
+
+      val testObject = facade.findById(correct_id)
+
+      assert(correct_id == testObject.id)
+    }
+  }
+
   "Call create" should {
     "insert data successfully" in {
-      val correct_lat = "13.7479752"
-      val correct_lng = "100.5836296"
+      val randomString = uuidService.getId
+      val correct_lat = 13.7479752f
+      val correct_lng = 100.5836296f
       val correct_name = "โรงพยาบาลกรุงเทพ"
       val facade = app.injector.instanceOf[HospitalRetrievalFacade]
 
