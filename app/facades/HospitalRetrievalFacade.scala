25579337521091a1b75a603ec20d97040621cc40
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-11 17:12:17 +0700

    เพิ่ม id และ เปลี่ยน String เป็น Float

--- app/facades/HospitalRetrievalFacade.scala
+++ app/facades/HospitalRetrievalFacade.scala
@@ -3,18 +3,25 @@ import javax.inject.{Inject, Singleton}
 
 import models.HospitalRetrieval
 import persists.HospitalRetrievalPersist
+import services.UuidService
 
 /**
   * Created by Siam yimyam on 11/8/2559.
   */
 @Singleton
-class HospitalRetrievalFacade @Inject()(persist: HospitalRetrievalPersist) {
+class HospitalRetrievalFacade @Inject()(uuidService: UuidService
+                                        , persist: HospitalRetrievalPersist) {
 
-  def create(lat: String
-             , lng: String
+  def findById(id: Long): HospitalRetrieval = {
+    persist.selectById(id).get
+  }
+
+  def create(lat: Float
+             , lng: Float
              , name: String): Boolean = {
     persist.insert(
-      lat = lat
+      id = uuidService.getId
+      , lat = lat
       , lng = lng
       , name = name
     )
