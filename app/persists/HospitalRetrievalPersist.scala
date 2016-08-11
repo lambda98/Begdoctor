6e5563fcb777811c9ea60c2a8265edb0183e2415
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-11 17:12:46 +0700

    เพิ่ม id และ เปลี่ยน String เป็น Float

--- app/persists/HospitalRetrievalPersist.scala
+++ app/persists/HospitalRetrievalPersist.scala
@@ -1,12 +1,16 @@
 package persists
 
+import models.HospitalRetrieval
+
 /**
   * Created by Siam yimyam on 10/8/2559.
   */
 trait HospitalRetrievalPersist {
 
-  def insert(lat: String
-             , lng: String
+  def selectById(id: Long): Option[HospitalRetrieval]
+  def insert(id: Long
+             ,lat: Float
+             , lng: Float
              , name: String): Boolean
 
 }
