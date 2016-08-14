265d0fbc7096cc0be05fb56d41cbf76ff7375870
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-15 00:29:32 +0700

    1. เปลียน lat เป็น latitude 2. เปลี่ยน lng เป็น longitude 3. เปลี่ยน String เป็น Float

--- test/models/HospitalSpec.scala
+++ test/models/HospitalSpec.scala
@@ -15,8 +15,8 @@ class HospitalSpec
       val correct_name = "Hae Song"
       val correct_url = "https://www.benin2009.com/wp-content/uploads/2015/11/hospital.png"
       val correct_doctorName = "Kang Moyeon"
-      val correct_latitude = "13.7854529"
-      val correct_longitude = "100.5736408"
+      val correct_latitude = 13.7854529f
+      val correct_longitude = 100.5736408f
       val correct_available_time = "10:00 - 17:00"
 
 
@@ -26,8 +26,8 @@ class HospitalSpec
         , name = correct_name
         , url = correct_url
         , doctorName = correct_doctorName
-        , lat = correct_latitude
-        , lng = correct_longitude
+        , latitude = correct_latitude
+        , longitude = correct_longitude
         , availableTime = correct_available_time
       )
 
@@ -35,8 +35,8 @@ class HospitalSpec
       assert(correct_name == testObject.name)
       assert(correct_url == testObject.url)
       assert(correct_doctorName == testObject.doctorName)
-      assert(correct_latitude == testObject.lat)
-      assert(correct_longitude == testObject.lng)
+      assert(correct_latitude == testObject.latitude)
+      assert(correct_longitude == testObject.longitude)
       assert(correct_available_time == testObject.availableTime)
     }
   }
