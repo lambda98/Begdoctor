8cb4fc542003328d9491387ef5e69be7e7388c70
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-14 23:56:16 +0700

    1. เปลียน lat เป็น latitude 2. เปลี่ยน lng เป็น longitude

--- app/models/HospitalRetrieval.scala
+++ app/models/HospitalRetrieval.scala
@@ -7,7 +7,7 @@ import utilities.Json
   * Created by Siam yimyam on 10/8/2559.
   */
 case class HospitalRetrieval(id: Long
-                             ,lat: Float
-                             , lng: Float
+                             , latitude: Float
+                             , longitude: Float
                              , name: String)
   extends Json
