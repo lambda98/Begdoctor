4feb28325618d868c0648b0f3c085446a5ae4155
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-15 00:00:37 +0700

    1. เปลียน lat เป็น latitude 2. เปลี่ยน lng เป็น longitude

--- test/models/HospitalRetrievalSpec.scala
+++ test/models/HospitalRetrievalSpec.scala
@@ -12,20 +12,20 @@ class HospitalRetrievalSpec
   "Create new HospitalRetrieval" should {
     "return HospitalRetrieval" in {
       val correct_id = 1L
-      val correct_lat = 13.7479752f
-      val correct_lng = 100.5836296f
+      val correct_latitude = 13.7479752f
+      val correct_longitude = 100.5836296f
       val correct_name = "โรงพยาบาลกรุงเทพ"
 
       val testObject = new HospitalRetrieval(
         id = correct_id
-        , lat = correct_lat
-        , lng = correct_lng
+        , latitude = correct_latitude
+        , longitude = correct_longitude
         , name = correct_name
       )
 
       assert(correct_id == testObject.id)
-      assert(correct_lat == testObject.lat)
-      assert(correct_lng == testObject.lng)
+      assert(correct_latitude == testObject.latitude)
+      assert(correct_longitude == testObject.longitude)
       assert(correct_name == testObject.name)
     }
   }
