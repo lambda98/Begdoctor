de767cb72b8ecb384b4c6222fb6fb9c7de1f04cb
SasimapornSmt <sasimaporn.smt@gmail.com>
2016-08-19 11:35:05 +0700

    ย้ายข้อมูล create , getByName , update จาก HospitalRetrieval ไปยัง Hospital

--- app/facades/HospitalFacade.scala
+++ app/facades/HospitalFacade.scala
@@ -4,12 +4,14 @@ import javax.inject.{Inject, Singleton}
 
 import models.{Hospital, HospitalList}
 import persists.HospitalPersist
+import services.UuidService
 
 /**
   * Created by champillon on 6/12/16.
   */
 @Singleton
-class HospitalFacade @Inject()(persist: HospitalPersist) {
+class HospitalFacade @Inject()(uuidService: UuidService
+                               , persist: HospitalPersist) {
 
   def findById(id: Long): Hospital = {
      persist.selectById(id).get
@@ -26,6 +28,49 @@ class HospitalFacade @Inject()(persist: HospitalPersist) {
     HospitalList(persist.selectByLocation(latitude, longitude).map(
       hospitalEntity => hospitalEntity.toModel()
     ))
+  }
+
+  def create(latitude: Float
+             , longitude: Float
+             , name: String): Boolean = {
+    persist.insert(
+      id = uuidService.getId
+      , latitude = latitude
+      , longitude = longitude
+      , name = name
+    )
+  }
+
+  def findByName(name: String): Hospital = {
+    persist.selectByName(name).getOrElse(null)
+  }
+
+  def update(latitude: Float
+             , longitude: Float
+             , name: String): Boolean = {
+    persist.update(
+      id = uuidService.getId
+      , latitude = latitude
+      , longitude = longitude
+      , name = name
+    )
+  }
+
+  def createOrUpdate( latitude: Float
+                      , longitude: Float
+                      , name: String): Boolean = {
+    val result = findByName(
+      name = name)
+    if(result == null){
+      create(latitude
+        , longitude
+        , name)
+    }
+    else{
+      update(latitude
+        , longitude
+        , name)
+    }
   }
 
 }
