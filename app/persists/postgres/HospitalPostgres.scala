5620467427081b1d30ec35e30c30a69f49cd29d3
champillon <passapong.t@gmail.com>
2016-08-21 11:59:54 +0700

    refactoring and change return type to HospitalEntity instead of Hospital

--- app/persists/postgres/HospitalPostgres.scala
+++ app/persists/postgres/HospitalPostgres.scala
@@ -14,26 +14,28 @@ import play.api.db.Database
 class HospitalPostgres @Inject()(db: Database)
   extends HospitalPersist {
 
-  override def selectById(id: Long): Option[Hospital] = db.withConnection { implicit conn =>
+  override def selectById(id: Long): Option[HospitalEntity] = db.withConnection { implicit conn =>
     val preparedStatement = conn.prepareStatement(SELECT_BY_ID)
     preparedStatement.setLong(1, id)
 
     val resultSet = preparedStatement.executeQuery
+
     resultSet.next match {
       case true => Some(parse(resultSet))
       case false => None
     }
   }
 
-  override def selectAll():  List[HospitalEntity] = db.withConnection { implicit conn =>
-
-    val preparedStatement = conn.prepareStatement(SELECT_ALL)
+  override def selectByName(name: String): Option[HospitalEntity] = db.withConnection { implicit conn =>
+    val preparedStatement = conn.prepareStatement(SELECT_BY_NAME)
+    preparedStatement.setString(1, name)
 
     val resultSet = preparedStatement.executeQuery
-    new Iterator[HospitalEntity] {
-      override def hasNext = resultSet.next()
-      override def next() = parseList(resultSet)
-    }.toList
+
+    resultSet.next match {
+      case true => Some(parse(resultSet))
+      case false => None
+    }
   }
 
   override def selectByLocation(latitude: Float
@@ -44,9 +46,22 @@ class HospitalPostgres @Inject()(db: Database)
     preparedStatement.setFloat(2, longitude)
 
     val resultSet = preparedStatement.executeQuery
+
     new Iterator[HospitalEntity] {
       override def hasNext = resultSet.next()
-      override def next() = parseList(resultSet)
+      override def next() = parse(resultSet)
+    }.toList
+  }
+
+  override def selectAll(): List[HospitalEntity] = db.withConnection { implicit conn =>
+
+    val preparedStatement = conn.prepareStatement(SELECT_ALL)
+
+    val resultSet = preparedStatement.executeQuery
+
+    new Iterator[HospitalEntity] {
+      override def hasNext = resultSet.next()
+      override def next() = parse(resultSet)
     }.toList
   }
 
@@ -59,20 +74,10 @@ class HospitalPostgres @Inject()(db: Database)
     preparedStatement.setFloat(2, latitude)
     preparedStatement.setFloat(3, longitude)
     preparedStatement.setString(4, name)
+
     preparedStatement.executeUpdate() match {
       case 1 => true
       case _ => false
-    }
-  }
-
-  override def selectByName(name: String): Option[Hospital] = db.withConnection { implicit conn =>
-    val preparedStatement = conn.prepareStatement(SELECT_BY_NAME)
-    preparedStatement.setString(1, name)
-
-    val resultSet = preparedStatement.executeQuery
-    resultSet.next match {
-      case true => Some(parse(resultSet))
-      case false => None
     }
   }
 
@@ -84,23 +89,14 @@ class HospitalPostgres @Inject()(db: Database)
     preparedStatement.setFloat(1, latitude)
     preparedStatement.setFloat(2, longitude)
     preparedStatement.setString(3, name)
+
     preparedStatement.executeUpdate() match {
       case 1 => true
       case _ => false
     }
   }
 
-  private[postgres] def parseList(resultSet: ResultSet): HospitalEntity = HospitalEntity(
-    id = resultSet.getLong("id")
-    , name = resultSet.getString("name")
-    , url = resultSet.getString("url")
-    , doctorName = resultSet.getString("doctor_name")
-    , latitude = resultSet.getFloat("latitude")
-    , longitude = resultSet.getFloat("longitude")
-    , availableTime = resultSet.getString("available_time")
-  )
-
-  private[postgres] def parse(resultSet: ResultSet): Hospital = Hospital(
+  private[postgres] def parse(resultSet: ResultSet): HospitalEntity = HospitalEntity(
     id = resultSet.getLong("id")
     , name = resultSet.getString("name")
     , url = resultSet.getString("url")
@@ -112,7 +108,7 @@ class HospitalPostgres @Inject()(db: Database)
 
   private val SELECT_BY_ID = "SELECT * FROM hospitals where id = ?"
 
-  private val SELECT_ALL = "SELECT * FROM hospitals"
+  private val SELECT_BY_NAME = "SELECT * FROM hospitals where name = ?"
 
   private val SELECT_BY_LOCATION = "SELECT * " +
     "                                      , round((point(latitude, longitude) <@> point(? , ?))::numeric, 2) as miles" +
@@ -120,10 +116,10 @@ class HospitalPostgres @Inject()(db: Database)
     "                               ORDER by miles ASC" +
     "                               limit 3;"
 
+  private val SELECT_ALL = "SELECT * FROM hospitals"
+
   private val INSERT = "INSERT INTO hospitals (id, latitude, longitude, name) VALUES (?, ?, ?, ?)"
 
   private val UPDATE = "UPDATE hospitals SET latitude = ?, longitude = ? where name = ?"
-
-  private val SELECT_BY_NAME = "SELECT * FROM hospitals where name = ?"
 
 }
