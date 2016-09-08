6c59412bd56046a14a6cb36da7685099422e794b
champillon <passapong.t@gmail.com>
2016-09-08 15:33:46 +0700

    create Form.scala to be centralized for FORM

---
+++ app/controllers/api/Form.scala
@@ -0,0 +1,49 @@
+package controllers.api
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+
+/**
+  * Created by champillon on 9/8/16.
+  */
+case class CreateBookingForm(name: String
+                             , surname: String
+                             , email: String
+                             , hospitalTimeId: Long)
+object CreateBookingForm {
+  val form = Form(
+    mapping(
+      "name" -> of[String],
+      "surname" -> of[String],
+      "email" -> of[String],
+      "hospitalTimeId" -> of[Long]
+    )(CreateBookingForm.apply)(CreateBookingForm.unapply)
+  )
+}
+
+case class CreateHospitalForm(latitude: Float
+                              , longitude: Float
+                              , name: String)
+object CreateHospitalForm {
+  val form = Form(
+    mapping(
+      "latitude" -> of[Float],
+      "longitude" -> of[Float],
+      "name" -> of[String]
+    ) (CreateHospitalForm.apply)(CreateHospitalForm.unapply)
+  )
+}
+
+case class UpdateHospitalForm (latitude: Float
+                               , longitude: Float
+                               , name: String)
+object UpdateHospitalForm {
+  val form = Form(
+    mapping(
+      "latitude" -> of[Float],
+      "longitude" -> of[Float],
+      "name" -> of[String]
+    ) (UpdateHospitalForm.apply) (UpdateHospitalForm.unapply)
+  )
+}
