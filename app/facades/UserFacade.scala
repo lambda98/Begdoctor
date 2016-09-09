9838138a87bddce3d1d0c2659ee5ba065ef2071a
champillon <passapong.t@gmail.com>
2016-09-09 22:13:31 +0700

    added mobile

--- app/facades/UserFacade.scala
+++ app/facades/UserFacade.scala
@@ -14,21 +14,23 @@ class UserFacade @Inject()(uuidService: UuidService
                            , persist: UserPersist) {
 
   def findById(id: Long): User = {
-    persist.selectById(id).get
+    persist.selectById(id).get.toModel()
   }
 
   def findByEmail(email: String): User = {
-    persist.selectByEmail(email).get
+    persist.selectByEmail(email).get.toModel()
   }
 
   def create(name: String
              , surname: String
-             , email: String): Boolean = {
+             , email: String
+             , mobile: String): Boolean = {
     persist.insert(
       id = uuidService.getId
       , name = name
       , surname = surname
       , email = email
+      , mobile = mobile
     )
   }
 
