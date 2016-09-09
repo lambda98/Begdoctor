8a1c3582be2ee2ab83696a11832e709f6871fa3e
champillon <passapong.t@gmail.com>
2016-09-09 22:14:32 +0700

    changed to UserEntity and added mobile

--- app/persists/UserPersist.scala
+++ app/persists/UserPersist.scala
@@ -1,16 +1,19 @@
 package persists
 
-import models.User
+import entities.UserEntity
 
 /**
   * Created by anawin on 6/23/2016 AD.
   */
 trait UserPersist {
 
-  def selectById(id: Long): Option[User]
-  def selectByEmail(email: String): Option[User]
+  def selectById(id: Long): Option[UserEntity]
+
+  def selectByEmail(email: String): Option[UserEntity]
+
   def insert(id: Long
              , name: String
              , surname: String
-             , email: String): Boolean
+             , email: String
+             , mobile: String): Boolean
 }
