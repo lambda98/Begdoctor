a36a10c130f902f067b22c7614ab6719e5e85eba
champillon <passapong.t@gmail.com>
2016-09-11 23:41:35 +0700

    added sypmtomId

--- app/persists/postgres/BookingPostgres.scala
+++ app/persists/postgres/BookingPostgres.scala
@@ -11,7 +11,7 @@ import play.api.db.Database
 /**
   * Created by anawin on 7/14/2016 AD.
   */
-class BookingPostgres @Inject() (db: Database)
+class BookingPostgres @Inject()(db: Database)
   extends BookingPersist {
 
   override def selectAll(): List[BookingEntity] = db.withConnection { implicit conn =>
@@ -20,6 +20,7 @@ class BookingPostgres @Inject() (db: Database)
     val resultSet = preparedStatement.executeQuery
     new Iterator[BookingEntity] {
       override def hasNext = resultSet.next()
+
       override def next() = parse(resultSet)
     }.toList
   }
@@ -31,6 +32,7 @@ class BookingPostgres @Inject() (db: Database)
     val resultSet = preparedStatement.executeQuery
     new Iterator[BookingEntity] {
       override def hasNext = resultSet.next()
+
       override def next() = parse(resultSet)
     }.toList
   }
@@ -42,6 +44,7 @@ class BookingPostgres @Inject() (db: Database)
     val resultSet = preparedStatement.executeQuery
     new Iterator[BookingEntity] {
       override def hasNext = resultSet.next()
+
       override def next() = parse(resultSet)
     }.toList
   }
@@ -49,30 +52,33 @@ class BookingPostgres @Inject() (db: Database)
   override def insert(id: Long
                       , userId: Long
                       , hospitalTimeId: Long
+                      , symptomId: Long
                       , status: String): Boolean = db.withConnection { implicit conn =>
 
     val preparedStatement = conn.prepareStatement(INSERT)
     preparedStatement.setLong(1, id)
     preparedStatement.setLong(2, userId)
     preparedStatement.setLong(3, hospitalTimeId)
-    preparedStatement.setString(4, status)
-    preparedStatement.setTimestamp(5, new Timestamp(new DateTime().getMillis))
+    preparedStatement.setLong(4, symptomId)
+    preparedStatement.setString(5, status)
+    preparedStatement.setTimestamp(6, new Timestamp(new DateTime().getMillis))
     preparedStatement.executeUpdate() match {
       case 1 => true
       case _ => false
     }
   }
 
-  private [postgres] def parse(resultSet: ResultSet): BookingEntity = BookingEntity(
+  private[postgres] def parse(resultSet: ResultSet): BookingEntity = BookingEntity(
     id = resultSet.getLong("id")
     , userId = resultSet.getLong("user_id")
     , hospitalTimeId = resultSet.getLong("hospital_time_id")
+    , symptomId = resultSet.getLong("symptom_id")
     , status = resultSet.getString("status")
-    , created =  new DateTime(resultSet.getTimestamp("created").getTime)
+    , created = new DateTime(resultSet.getTimestamp("created").getTime)
   )
 
   private val SELECT_ALL = "SELECT * FROM booking"
   private val SELECT_BY_ID = "SELECT * FROM booking where id = ?"
   private val SELECT_BY_USER_ID = "SELECT * FROM booking where user_id = ?"
-  private val INSERT = "INSERT INTO booking (id, user_id, hospital_time_id, status, created) VALUES (?, ?, ?, ?, ?)"
+  private val INSERT = "INSERT INTO booking (id, user_id, hospital_time_id, symptom_id, status, created) VALUES (?, ?, ?, ?, ?, ?)"
 }
