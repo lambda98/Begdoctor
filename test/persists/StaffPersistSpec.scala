95aa1754ee2fe448247d21e61b3deb6450f85445
champillon <passapong.t@gmail.com>
2016-09-11 23:45:48 +0700

    refactored

--- test/persists/StaffPersistSpec.scala
+++ test/persists/StaffPersistSpec.scala
@@ -1,25 +1,20 @@
 package persists
 
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
-import javax.inject.Inject
 
 import services.UuidService
-import com.github.t3hnar.bcrypt._
-import entities.StaffEntity
-import persists.postgres.StaffPostgres
 
 /**
   * Created by anawin on 8/6/2016 AD.
   */
-class StaffPersistSpec @Inject()(uuidService: UuidService)
+class StaffPersistSpec
   extends PlaySpec
     with OneAppPerSuite {
 
   "Call selectByUsername" should {
     "return staff data" in {
-      val correct_username = "choiwoogeun"
-      val correct_password = "password"
       val persist = app.injector.instanceOf[StaffPersist]
+      val correct_username = "choiwoogeun"
 
       val testObject = persist.selectByUserName(correct_username)
 
@@ -29,23 +24,24 @@ class StaffPersistSpec @Inject()(uuidService: UuidService)
 
   "Call insert staff" should {
     "insert staff data successfully" in {
-      val randomId = uuidService.getId
-      val correct_id = randomId
+      val persist = app.injector.instanceOf[StaffPersist]
+      val uuidService = app.injector.instanceOf[UuidService]
+      val correct_id = uuidService.getId
       val correct_name = "Im"
       val correct_surname = "Gwang-nam"
       val correct_email = "imgwangnam@mail.com"
       val correct_username = "imgwangnam"
       val correct_password = "picolo"
       val correct_hospitalId = uuidService.getId
-      val persist = app.injector.instanceOf[StaffPersist]
+
 
       val testObject = persist.insert(correct_id
-                                      , correct_name
-                                      , correct_surname
-                                      , correct_email
-                                      , correct_username
-                                      , correct_password
-                                      , correct_hospitalId)
+        , correct_name
+        , correct_surname
+        , correct_email
+        , correct_username
+        , correct_password
+        , correct_hospitalId)
 
       assert(testObject)
     }
