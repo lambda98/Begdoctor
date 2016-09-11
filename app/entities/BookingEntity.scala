3bb12826680f8d7b8294bec6c6e45e3ca7f56eba
champillon <passapong.t@gmail.com>
2016-09-11 23:38:50 +0700

    added symptomId

--- app/entities/BookingEntity.scala
+++ app/entities/BookingEntity.scala
@@ -6,16 +6,18 @@ import org.joda.time.DateTime
 /**
   * Created by anawin on 7/14/2016 AD.
   */
-case class BookingEntity (id: Long
-                          , userId: Long
-                          , hospitalTimeId: Long
-                          , status: String
-                          , created: DateTime) {
+case class BookingEntity(id: Long
+                         , userId: Long
+                         , hospitalTimeId: Long
+                         , symptomId: Long
+                         , status: String
+                         , created: DateTime) {
 
   def toModel(): Booking = Booking(
     id = id
     , userId = userId
     , hospitalTimeId = hospitalTimeId
+    , symptomId = symptomId
     , status = status
     , created = created
   )
